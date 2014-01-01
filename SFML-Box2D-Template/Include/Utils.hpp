fe0052e4da74e512130319adcde9bec0d0dcd574
jhpy1024 <jhpy1024@gmail.com>
2014-01-01 13:56:48 +0000

    Added utils file which contains helper functions.

---
+++ SFML-Box2D-Template/Include/Utils.hpp
@@ -0,0 +1,7 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+float pixelsToMeters(float pixels);
+float metersToPixels(float meters);
+
+#endif
---
+++ SFML-Box2D-Template/Source/Utils.cpp
@@ -0,0 +1,11 @@
+#include "../Include/Utils.hpp"
+
+float pixelsToMeters(float pixels)
+{
+	return pixels / 100.f;
+}
+
+float metersToPixels(float meters)
+{
+	return meters * 100.f;
+}
